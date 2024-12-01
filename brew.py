b09882c309ad40ec7b48567617a65bfad93eea7a
w0rng <work@w0rng.ru>
2024-12-01 10:05:38 +0700

    refactoring + mas

--- brew.py
+++ brew.py
@@ -1,104 +1,169 @@
-import os, platform, subprocess, dotbot, sys
+import os, subprocess, dotbot, sys
+from typing import Iterable, Mapping, Any
+
+Context = Mapping[str, Any]
+
 
 class Brew(dotbot.Plugin):
-    _brewDirective = "brew"
-    _caskDirective = "cask"
-    _tapDirective = "tap"
-    _brewFileDirective = "brewfile"
+    default_context = {
+        "brew": {
+            "stdin": False,
+            "stderr": False,
+            "stdout": False,
+            "force_intel": False,
+        },
+        "cask": {
+            "stdin": False,
+            "stderr": False,
+            "stdout": False,
+            "force_intel": False,
+        },
+        "brewfile": {
+            "stdin": True,
+            "stderr": True,
+            "stdout": True,
+            "force_intel": False,
+        },
+    }
+
+    def __init__(self, *args, **kwargs):
+        self.directives = {
+            "brew": self._brew,
+            "cask": self._cask,
+            "tap": self._tap,
+            "brewfile": self._brew_file,
+            "app-store": self._mas,
+        }
+
+        super().__init__(*args, **kwargs)
 
     def can_handle(self, directive):
-        return directive in (self._tapDirective, self._brewDirective, self._caskDirective, self._brewFileDirective)
+        return directive in self.directives
 
-    def handle(self, directive, data):
-        if directive == self._tapDirective:
-            self._bootstrap_brew()
-            return self._tap(data)
-        if directive == self._brewDirective:
-            self._bootstrap_brew()
-            return self._process_data("brew install", data)
-        if directive == self._caskDirective:
-            if sys.platform.startswith("darwin"):
-                self._bootstrap_cask()
-                return self._process_data("brew install --cask", data)
-            else:
-                return True
-        if directive == self._brewFileDirective:
-            self._bootstrap_brew()
-            self._bootstrap_cask()
-            return self._install_bundle(data)
-        raise ValueError('Brew cannot handle directive %s' % directive)
+    def handle(self, directive: str, data: Iterable[str]) -> bool:
+        user_context = self._context.defaults().get(directive, {})
+        local_context = self.default_context.get(directive, {})
+        context = {**local_context, **user_context}
 
-    def _tap(self, tap_list):
-        cwd = self._context.base_directory()
-        log = self._log
-        with open(os.devnull, 'w') as devnull:
-            stdin = stdout = stderr = devnull
-            for tap in tap_list:
-                log.info("Tapping %s" % tap)
-                cmd = "brew tap %s" % (tap)
-                result = subprocess.call(cmd, shell=True, cwd=cwd)
+        return self.directives[directive](data, context)
 
-                if result != 0:
-                    log.warning('Failed to tap [%s]' % tap)
-                    return False
-            return True
+    def _tap(self, tap_list: Iterable[str], context: Context) -> bool:
+        if not self._brew_exist(context):
+            if not self._install_brew(context):
+                self._log.error("Failed to install brew")
+                return False
 
-    def _process_data(self, install_cmd, data):
-        success = self._install(install_cmd, data)
-        if success:
-            self._log.info('All packages have been installed')
-        else:
-            self._log.error('Some packages were not installed')
-        return success
+        result = True
 
-    def _install(self, install_cmd, packages_list):
-        cwd = self._context.base_directory()
-        log = self._log
-        with open(os.devnull, 'w') as devnull:
-            stdin = stdout = stderr = devnull
-            for package in packages_list:
-                if install_cmd == 'brew install':
-                    cmd = "brew ls --versions %s" % package
-                else:
-                    cmd = "brew cask ls --versions %s" % package
-                isInstalled = subprocess.call(cmd, shell=True, stdin=stdin, stdout=stdout, stderr=stderr, cwd=cwd)
-                if isInstalled != 0:
-                    log.info("Installing %s" % package)
-                    cmd = "%s %s" % (install_cmd, package)
-                    result = subprocess.call(cmd, shell=True, cwd=cwd)
-                    if result != 0:
-                        log.warning('Failed to install [%s]' % package)
-                        return False
-            return True
+        for tap in tap_list:
+            self._log.info(f"Tapping {tap}")
+            cmd_result = self._invoke_shell_command(f"brew tap {tap}", context)
+            if cmd_result != 0:
+                self._log.warning(f"Failed to tap [{tap}]")
+                result = False
 
-    def _install_bundle(self, brew_files):
-        cwd = self._context.base_directory()
-        log = self._log
-        with open(os.devnull, 'w') as devnull:
-            stdin = stdout = stderr = devnull
-            for f in brew_files:
-                log.info("Installing from file %s" % f)
-                cmd = "brew bundle --file=%s" % f
-                result = subprocess.call(cmd, shell=True, cwd=cwd)
+        return result
 
-                if result != 0:
-                    log.warning('Failed to install file [%s]' % f)
-                    return False
-            return True
+    def _brew(self, packages_list: Iterable[str], context: Context):
+        if not self._brew_exist(context):
+            if not self._install_brew(context):
+                self._log.error("Failed to install brew")
+                return False
 
-    def _bootstrap(self, cmd):
-        with open(os.devnull, 'w') as devnull:
-            stdin = stdout = stderr = devnull
-            subprocess.call(cmd, shell=True, stdin=stdin, stdout=stdout, stderr=stderr,
-                            cwd=self._context.base_directory())
+        result = True
 
-    def _bootstrap_brew(self):
+        for pkg in packages_list:
+            self._log.info(f"Install {pkg}")
+            success = self._invoke_shell_command(f"brew install {pkg}", context)
+
+            if not success:
+                self._log.warning(f"Failed to install [{pkg}]")
+                result = False
+
+        return result
+
+    def _brew_file(self, brew_files: Iterable[str], context: Context):
+        if not self._brew_exist(context):
+            if not self._install_brew(context):
+                self._log.error("Failed to install brew")
+                return False
+
+        result = True
+
+        for file in brew_files:
+            self._log.info(f"Installing from file {file}")
+            success = self._invoke_shell_command(f"brew bundle --verbose --file={file}", context)
+
+            if not success:
+                self._log.warning(f"Failed to install file [{file}]")
+                result = False
+
+        return result
+
+    def _cask(self, packages: Iterable[str], context: Context) -> bool:
+        if not self._brew_exist(context):
+            if not self._install_brew(context):
+                self._log.error("Failed to install brew")
+                return False
+
+        result = True
+
+        for pkg in packages:
+            self._log.info(f"Install cask {pkg}")
+            success = self._invoke_shell_command(
+                f"brew install --cask {pkg} || brew ls --cask --versions {pkg}",
+                context,
+            )
+            if not success:
+                self._log.warning(f"Failed to install cask [{pkg}]")
+                result = False
+
+        return result
+
+    def _mas(self, apps: Iterable[str], context: Context):
+        if not self._mas_exist(context):
+            if not self._install_mas(context):
+                self._log.error("Failed to install mas")
+                return False
+
+        result = True
+
+        for app in apps:
+            self._log.info(f"Install app {app} for appStore")
+            success = self._invoke_shell_command(f"mas install {app}", context)
+
+            if success != 0:
+                self._log.warning(f"Failed to install app [{app}]")
+                result = False
+
+        return result
+
+    def _brew_exist(self, context: Context) -> bool:
+        return self._invoke_shell_command("hash brew", context) == 0
+
+    def _mas_exist(self, context: Context) -> bool:
+        return self._invoke_shell_command("hash mas", context) == 0
+
+    def _install_brew(self, context: Context) -> bool:
         link = "https://raw.githubusercontent.com/Homebrew/install/master/install.sh"
         cmd = """hash brew || /bin/bash -c "$(curl -fsSL {0})";
               brew update""".format(link)
-        self._bootstrap(cmd)
 
-    def _bootstrap_cask(self):
-        self._bootstrap_brew()
-        cmd = "brew tap caskroom/cask"
-        self._bootstrap(cmd)
+        return self._invoke_shell_command(cmd, context)
+
+    def _install_mas(self, context: Context) -> bool:
+        return self._brew(["mas"], context)
+
+    def _invoke_shell_command(self, cmd: str, context: Context) -> bool:
+        with open(os.devnull, "w") as devnull:
+            if context["force_intel"]:
+                cmd = "arch --x86_64 " + cmd
+
+            return subprocess.call(
+                cmd,
+                shell=True,
+                cwd=self._context.base_directory(),
+                stdin=devnull if context["stdin"] else None,
+                stdout=devnull if context["stdout"] else None,
+                stderr=devnull if context["stderr"] else None,
+            ) == 0
